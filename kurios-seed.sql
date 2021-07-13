b88d4df4e363b8dc4742db35a2fac1db0af83547
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-07-13 14:16:31 -0700

    added card data

--- kurios-seed.sql
+++ kurios-seed.sql
@@ -2,6 +2,17 @@
 -- Create mock users
 -- Create mock listings
 
+INSERT INTO tutorials (name, description)
+VALUES('Beginner’s Guide', 'Learn the basics of cryptocurrencies'),
+      ('General','Background information on cryptocurrencies'),
+      ('ADA', 'Learn about trading and how to time investments'),
+      ('BTC', 'Learn about Bitcoin the worlds first cryptocurrency'),
+      ('ETH', 'Learn about Ethereum and decentralization'),
+      ('ADA','Learn about Cardano and the basics of staking'),
+      ('DOGE', 'Learn about Dogecoin and risk assessment'),
+      ('DOT', 'Learn about Polkadot and blockchain'),
+      ('XMR', 'Learn about Monero and anonymity');
+
 INSERT INTO users (username, first_name, last_name, email, password)
 VALUES (
   'jlo',
@@ -31,11 +42,13 @@ VALUES(2,10000,4);
 INSERT INTO wallet (user_id, btc, eth, dot)
 VALUES(3,100000, 1, 5);
 
-INSERT INTO tutorials (name, description)
-VALUES('Bitcoin','2fsfdsfsd'),('ETH','ddfsdfsd'),('Generic','5sdfdsfds');
 
 INSERT INTO completed_tutorials(user_id, tutorial_id)
 VALUES(1,1), (1,2), (1,3), (2,1), (2,2),(2,3), (3,1), (3,2), (3,3);
+
+
+
+
 
 -- UPDATE EXAMPLE
 -- UPDATE completed_tutorials
@@ -51,3 +64,13 @@ VALUES(1,1), (1,2), (1,3), (2,1), (2,2),(2,3), (3,1), (3,2), (3,3);
 
 
 
+
+    --   (“Beginner’s Guide”, “Learn the basics of cryptocurrencies”)
+    --   (“General”,“Background information on cryptocurrencies”),
+    --   (“ADA”, “Learn about trading and how to time investments”),
+    --   (“BTC”, “Learn about Bitcoin the worlds first cryptocurrency”),
+    --   (“ETH”, “Learn about Ethereum and decentralization”),
+    -- (“ADA”,“Learn about Cardano and the basics of staking”),
+    -- (“DOGE”, “Learn about Dogecoin and risk assessment”),
+    -- (“DOT”, “Learn about Polkadot and blockchain”),
+    -- (“XMR”, “Learn about Monero and anonymity”);
