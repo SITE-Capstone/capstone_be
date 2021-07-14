40a916fb26e159575ff13f7d826e8b48fa40fecd
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-07-14 09:45:37 -0700

    cleaned up tutorials code

--- models/tutorial.js
+++ models/tutorial.js
@@ -2,44 +2,47 @@ const { BadRequestError } = require("../utils/errors");
 const db = require("../db");
 
 class Tutorial {
+
   static makePublicCompletedTutorials(completed_tutorials) {
     return completed_tutorials
   }
+  
+  
 
-  // Creates a Completed tutorials list
+
+  // Creates & assigns a Completed tutorials list to a user_id 
   static async generateCompletedTutorials(user_id) {
-    //We only need a user Id since all tutorials will default to false
-    const requiredFields = ["user_id"]
+  const requiredFields = ["user_id"]  
+  if (!user_id) {
+      throw new BadRequestError(`Missing ${user_id} in request body.`);
+  }
 
-      if (!user_id) {
-        throw new BadRequestError(`Missing ${user_id} in request body.`);
-    }
-
-// const existingCompletedTutorials = await Tutorial.fetchCompletedTutorialsByUserId(user\_id);
-// if (existingCompletedTutorials) {
-//   throw new BadRequestError(`A Completed Tutorials already exists with User\_ID: ${user\_id}`);
-// }
-
-    const completedTutorialsResult = await db.query(
-      ` INSERT INTO completed_tutorials (user_id, tutorial_id)
-        VALUES ($1,1),($1,2),($1,3),($1,4),($1,5),($1,6),($1,7),($1,8),($1,9)
-        RETURNING completed_tutorials;
-      `,
+  //Will default to false
+  const completedTutorialsResult = await db.query(
+    ` 
+    INSERT INTO completed_tutorials (user_id, tutorial_id)
+    VALUES ($1,1),($1,2),($1,3),($1,4),($1,5),($1,6),($1,7),($1,8),($1,9)
+    RETURNING completed_tutorials;
+    `,
       [user_id]
     );
 
     const completed_tutorials = completedTutorialsResult.rows;
     console.log("Tutorial class->Generated", Tutorial.makePublicCompletedTutorials(completed_tutorials))
-    return Tutorial.makePublicCompletedTutorials(completed_tutorials);
+    return completed_tutorials;
   }
 
-  // fetches the CompletedTutorials with the user_id_
+
+
+
+
+  // fetches the CompletedTutorials with the user_id
   static async fetchCompletedTutorialsByUserId(user_id) {
     if (!user_id) {
       throw new BadRequestError("No user_id provided");
     }
-      const query = `SELECT * FROM completed_tutorials WHERE user_id = $1`;
-    const result = await db.query(query, [user_id]);
+    const fetchQuery = `SELECT * FROM completed_tutorials WHERE user_id = $1`;
+    const result = await db.query(fetchQuery, [user_id]);
 
 
     const completed_tutorials = result.rows;
@@ -47,36 +50,38 @@ class Tutorial {
     return completed_tutorials;
   }
 
+
+
+
   static async editCompletedTutorials(user_id, tutorial_id, completed) {
     const requiredFields = ["user_id", "tutorial_id", "completed"]
 
-      if (!user_id) {
-        throw new BadRequestError(`Missing ${user_id} in request body.`);
+    if (!user_id) {
+      throw new BadRequestError(`Missing ${user_id} in request body.`);
     }
-      if (!tutorial_id) {
-        throw new BadRequestError(`Missing ${tutorial_id} in request body.`);
+    if (!tutorial_id) {
+      throw new BadRequestError(`Missing ${tutorial_id} in request body.`);
     }
 
-
-    const editCompletedTutorials = await db.query(
-      ` 
-        UPDATE completed_tutorials
-        SET completed = $3
-        WHERE user_id = $1 AND tutorial_id = $2;
-      `,
-      [user_id, tutorial_id, completed]
-    );
-    const completedTutorialsResult = await db.query(
-      ` 
-        SELECT * FROM completed_tutorials
-        WHERE user_id = $1 AND tutorial_id = $2;
-      `,
-      [user_id, tutorial_id]
-    );
+    //First db.query edits the table
+    //Second db.query returns the table
+    const editQuery =
+    ` 
+    UPDATE completed_tutorials
+    SET completed = $3
+    WHERE user_id = $1 AND tutorial_id = $2;
+    `;
+    const resultQuery =
+    ` 
+    SELECT * FROM completed_tutorials
+    WHERE user_id = $1 AND tutorial_id = $2;
+    `;
+    const editCompletedTutorials = await db.query(editQuery, [user_id, tutorial_id, completed]);
+    const completedTutorialsResult = await db.query(resultQuery, [user_id, tutorial_id]);
 
     const completed_tutorials = completedTutorialsResult.rows;
-    console.log("Tutorial class->completed?", Tutorial.makePublicCompletedTutorials(completed_tutorials))
-    return Tutorial.makePublicCompletedTutorials(completed_tutorials);
+    console.log("Tutorial class->editCompletedTutorials", Tutorial.makePublicCompletedTutorials(completed_tutorials))
+    return completed_tutorials;
   }
 
 }
