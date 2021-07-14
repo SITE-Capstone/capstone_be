9db053ef90882366796218279550d99a3f9432b5
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-07-13 20:47:07 -0700

    finished the tutorial get request after codebase broke

--- models/tutorial.js
+++ models/tutorial.js
@@ -3,26 +3,22 @@ const db = require("../db");
 
 class Tutorial {
   static makePublicCompletedTutorials(completed_tutorials) {
-    return {
-    //   user_id: completed_tutorials.user_id,
-    completed_tutorials: completed_tutorials,
-    };
+    return completed_tutorials
   }
 
   // Creates a Completed tutorials list
   static async generateCompletedTutorials(user_id) {
     //We only need a user Id since all tutorials will default to false
-    const requiredFields = ["user_id"];
+    const requiredFields = ["user_id"]
 
       if (!user_id) {
         throw new BadRequestError(`Missing ${user_id} in request body.`);
     }
 
-    const existingCompletedTutorials = await Tutorial.fetchCompletedTutorialsByUserId(user_id);
-
-    if (existingCompletedTutorials) {
-      throw new BadRequestError(`A Completed Tutorials already exists with User_ID: ${user_id}`);
-    }
+// const existingCompletedTutorials = await Tutorial.fetchCompletedTutorialsByUserId(user\_id);
+// if (existingCompletedTutorials) {
+//   throw new BadRequestError(`A Completed Tutorials already exists with User\_ID: ${user\_id}`);
+// }
 
     const completedTutorialsResult = await db.query(
       ` INSERT INTO completed_tutorials (user_id, tutorial_id)
@@ -33,26 +29,23 @@ class Tutorial {
     );
 
     const completed_tutorials = completedTutorialsResult.rows;
-    console.log("Tutorial class", Tutorial.makePublicCompletedTutorials(completed_tutorials))
+    console.log("Tutorial class->Generated", Tutorial.makePublicCompletedTutorials(completed_tutorials))
     return Tutorial.makePublicCompletedTutorials(completed_tutorials);
   }
 
-  // fetches the CompletedTutorials with the user_id
+  // fetches the CompletedTutorials with the user_id_
   static async fetchCompletedTutorialsByUserId(user_id) {
     if (!user_id) {
       throw new BadRequestError("No user_id provided");
     }
-
-    const query = `SELECT * FROM completed_tutorials WHERE user_id = $1`;
-
+      const query = `SELECT * FROM completed_tutorials WHERE user_id = $1`;
     const result = await db.query(query, [user_id]);
 
-    const completed_tutorials = result.rows[0];
 
+    const completed_tutorials = result.rows;
+    console.log("Tutorial class->Fetched", Tutorial.makePublicCompletedTutorials(completed_tutorials))
     return completed_tutorials;
   }
 }
 
-
 module.exports = Tutorial;
-
