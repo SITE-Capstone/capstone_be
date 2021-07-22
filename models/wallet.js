e14dd20883e12c1b30d6247ffa0c2fbee21e218f
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-07-22 09:02:23 -0700

    Fixed #34

--- models/wallet.js
+++ models/wallet.js
@@ -129,7 +129,7 @@ class Wallet {
     let buying_quantity=order.quantity
     let selling_quantity=order.quantity
     if (order.type===0){
-      selling_quantity=Math.floor(order.quantity*order.price)
+      selling_quantity=Math.floor(order.quantity*order.price)+1
     }    
     if (order.type===1){
 
@@ -141,6 +141,12 @@ class Wallet {
     console.log("140",order.selling_id, currency2[order.selling_id],"->", order.buying_id, currency1[order.buying_id])
     
     
+    if(order.quantity<=0){
+      throw new BadRequestError(`Please set the amount to be greater than 0`);
+    }
+    if(currency2[order.selling_id]<1){
+      throw new BadRequestError(`You have ran out of ${order.selling_id}.`);
+    }
     if(currency2[order.selling_id]<selling_quantity){
       throw new BadRequestError(`Not enough ${order.selling_id} to purchase.`);
     }
