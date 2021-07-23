91028b295b066adce8a2af0d8c076997d01ea3b9
vZireael <75550025+vZireael@users.noreply.github.com>
2021-07-23 11:43:47 -0400

    added route and model for fetching the completed status of a single tutorial by id

--- routes/tutorials.js
+++ routes/tutorials.js
@@ -6,51 +6,69 @@ const router = express.Router();
 
 // Gets the completed statuses of all the tutorials in completed_tutorials per user_id
 router.get("/cards", async (req, res, next) => {
-    try {
+  try {
+    //Model Functions
+    const tutorialCards = await Tutorial.fetchTutorialCards();
+    const publicTutorials = await Tutorial.makePublicTutorialCards(tutorialCards);
 
-      //Model Functions
-      const tutorialCards = await Tutorial.fetchTutorialCards();
-      const publicTutorials = await Tutorial.makePublicTutorialCards(tutorialCards)
-
-      return res.status(200).json({ Tutorials: publicTutorials });
+    return res.status(200).json({ Tutorials: publicTutorials });
   } catch (err) {
-    next(err)
-    }
+    next(err);
+  }
 });
 
 //Edits a completed tutorial to true || false based on user_id & tutorial_id
 router.put("/completed", security.requireAuthenticatedUser, async (req, res, next) => {
   try {
-      //Defining the parameters
-      const { tutorial_id, completed} = req.body
-      const { username } = res.locals.user;
-      const user = await User.fetchUserByUsername(username);
-      const { id } = user;
-      
-      //Model Functions
-      const completedTutorials = await Tutorial.editCompletedTutorials(id, tutorial_id, completed);
-      const publicCompletedTutorials = await Tutorial.makePublicCompletedTutorials(completedTutorials)
-      
-      return res.status(200).json({ completedTutorials: publicCompletedTutorials });
-    } catch (err) {
-        next(err);
-    }
+    //Defining the parameters
+    const { tutorial_id, completed } = req.body;
+    const { username } = res.locals.user;
+    const user = await User.fetchUserByUsername(username);
+    const { id } = user;
+
+    //Model Functions
+    const completedTutorials = await Tutorial.editCompletedTutorials(id, tutorial_id, completed);
+    const publicCompletedTutorials = await Tutorial.makePublicCompletedTutorials(completedTutorials);
+
+    return res.status(200).json({ completedTutorials: publicCompletedTutorials });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Gets the completed statuses of all the tutorials in completed_tutorials per user_id
 router.get("/completed", security.requireAuthenticatedUser, async (req, res, next) => {
-    try {
+  try {
+    //Defining the parameters
+    const { username } = res.locals.user;
+    const user = await User.fetchUserByUsername(username);
+    const { id } = user;
 
-      //Defining the parameters
-      const { username } = res.locals.user;
-      const user = await User.fetchUserByUsername(username);
-      const { id } = user;
-      
-      //Model Functions
-      const completedTutorials = await Tutorial.fetchCompletedTutorialsByUserId(id);
-      const publicCompletedTutorials = await Tutorial.makePublicCompletedTutorials(completedTutorials)
+    //Model Functions
+    const completedTutorials = await Tutorial.fetchCompletedTutorialsByUserId(id);
+    const publicCompletedTutorials = await Tutorial.makePublicCompletedTutorials(completedTutorials);
 
-      return res.status(200).json({ completedTutorials: publicCompletedTutorials });
+    return res.status(200).json({ completedTutorials: publicCompletedTutorials });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Gets the completed statuses of one tutorial in completed_tutorials per user_id and tutorial_id
+router.get("/single", security.requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    //Defining the parameters
+    console.log("reqBody", req.query);
+    const { tutorial_id } = req.query;
+    const { username } = res.locals.user;
+    const user = await User.fetchUserByUsername(username);
+    const { id } = user;
+
+    //Model Functions
+    const completedTutorials = await Tutorial.fetchSingleTutorial(id, tutorial_id);
+    const publicCompletedTutorial = await Tutorial.makePublicCompletedTutorials(completedTutorials);
+
+    return res.status(200).json({ completedTutorials: publicCompletedTutorial });
   } catch (err) {
     next(err);
   }
