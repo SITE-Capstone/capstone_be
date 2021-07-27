81d8ef388c6861a52733cb8e99f5ec177fdd4644
vZireael <75550025+vZireael@users.noreply.github.com>
2021-07-27 16:21:14 -0400

    added cron scheduler for yearly coin data and updated currentPrice, daily, and weekly schedulers. Added keys to .env

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -4,7 +4,7 @@ const apiCall = require("./apiCalls");
 module.exports = {
   coinRequestCrons: () => {
     cron.schedule("*/5 * * * * *", async () => {
-      // every 5 seconds
+      // every 5 seconds... change to 15 seconds after testing
       apiCall.getCoinCurrentPrice("BTC");
     });
     cron.schedule("*/15 * * * *", async () => {
@@ -14,6 +14,10 @@ module.exports = {
     cron.schedule("0 */4 * * *", async () => {
       // every 4 hours
       apiCall.getCoinWeeklyPriceHistory("BTC");
+    });
+    cron.schedule("0 11 * * *", async () => {
+      // once a day at 11 am
+      apiCall.getCoinYearlyPriceHistory("BTC");
     });
   },
   // hourlyCron: () => {
