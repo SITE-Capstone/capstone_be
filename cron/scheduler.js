b2f4ddf32f05cbed9daee0e7999fba1ccf8bc38f
vZireael <75550025+vZireael@users.noreply.github.com>
2021-08-03 14:51:30 -0400

    modified scheduler

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -1,28 +1,28 @@
 const cron = require("node-cron");
 const Price = require("../models/price");
 const apiCall = require("./apiCalls");
-const data = require("./data")
+const data = require("./data");
 
 const symbols = ["BTC", "ETH", "ADA", "DOGE", "DOT", "XMR"];
 
 module.exports = {
   coinRequestCrons: () => {
-    cron.schedule("*/5 * * * * *", async () => {
+    cron.schedule("* * * * *", async () => {
       // every 5 seconds... change to 15 seconds after testing
-      data.updateCurrentPriceForAll()
+      data.updateCurrentPriceForAll();
     });
     cron.schedule("*/15 * * * *", async () => {
       // every 15th minute
 
-      data.updateDailyForAll()
+      data.updateDailyForAll();
     });
     cron.schedule("0 */4 * * *", async () => {
       // every 4 hours
-      data.updateWeeklyForAll()
+      data.updateWeeklyForAll();
     });
     cron.schedule("0 11 * * *", async () => {
       // once a day at 11 am
-      data.updateYearlyForAll()
+      data.updateYearlyForAll();
     });
   },
 };
