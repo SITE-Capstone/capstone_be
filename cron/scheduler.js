123f4a640178b2dcb7d30edbd85c37bfe56db0c5
vZireael <75550025+vZireael@users.noreply.github.com>
2021-08-04 13:08:13 -0400

    time

--- cron/scheduler.js
+++ cron/scheduler.js
@@ -8,19 +8,18 @@ const symbols = ["BTC", "ETH", "ADA", "DOGE", "DOT", "XMR"];
 module.exports = {
   coinRequestCrons: () => {
     cron.schedule("* * * * *", async () => {
-      // every 5 seconds... change to 15 seconds after testing
+      // every minute seconds... change to 15 seconds after testing
       data.updateCurrentPriceForAll();
     });
     cron.schedule("*/15 * * * *", async () => {
       // every 15th minute
-
       data.updateDailyForAll();
     });
     cron.schedule("0 */4 * * *", async () => {
       // every 4 hours
       data.updateWeeklyForAll();
     });
-    cron.schedule("0 */12 * * *", async () => {
+    cron.schedule("10 17 * * *", async () => {
       // every 12 hours
       data.updateYearlyForAll();
     });
