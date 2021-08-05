f49b5c960a365b729c649c3e61bff08d7d1e3a15
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-08-05 11:48:25 -0700

    added seed data for the articles table

--- kurios-schema.sql
+++ kurios-schema.sql
@@ -74,6 +74,14 @@ CREATE TABLE current_price(
   time              TEXT NOT NULL
 );
 
+CREATE TABLE news_articles(
+  id                SERIAL PRIMARY KEY, 
+  coin_id           TEXT NOT NULL,
+  headline          TEXT NOT NULL,
+  time              TEXT NOT NULL,
+  source            TEXT NOT NULL,
+  image_url         TEXT NOT NULL
+)
 
 
 
